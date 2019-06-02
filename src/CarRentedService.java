cf551f8713f243d9f87ed9a2a200ed8aae0f8426
teixeiramarcia <filipa.teixeira98@gmail.com>
2019-06-02 16:51:07 +0100

    rating do carro e do proprietário

--- src/CarRentedService.java
+++ src/CarRentedService.java
@@ -1,5 +1,6 @@
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
 import java.io.IOException;
 import java.time.LocalDate;
@@ -8,6 +9,11 @@ public class CarRentedService {
 
     @FXML
     private Button goBack;
+    @FXML
+    private TextField carRating;
+    @FXML
+    private TextField propRating;
+
 
     public void goBack() throws IOException {
         Rental rental = UMCarroJa.getService().getOngoingRental();
@@ -17,4 +23,17 @@ public class CarRentedService {
         UMCarroJa.getService().setOngoingRental(null);
         Redirect.redirectTo(goBack, "views/homeClient.fxml");
     }
+
+    public void rateCar(){
+        int rating = Integer.parseInt(carRating.getText());
+        UMCarroJa.getService().getOngoingRental().getRentedCar().setRating(rating);
+    }
+
+    public void rateProp() {
+        int rating = Integer.parseInt(propRating.getText());
+        UMCarroJa.getService().getOngoingRental().getRentedCar().getProprietary().setRating(rating);
+    }
+
+
+
 }
